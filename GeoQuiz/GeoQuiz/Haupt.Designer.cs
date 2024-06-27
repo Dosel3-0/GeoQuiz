8102ce6ba12a5f86a3c0d270de243d1f5c086cbe
Dosel3-0 <167423357+Dosel3-0@users.noreply.github.com>
2024-06-27 20:24:21 +0200

    Add files via upload

--- GeoQuiz/GeoQuiz/Haupt.Designer.cs
+++ GeoQuiz/GeoQuiz/Haupt.Designer.cs
@@ -30,12 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Haupt));
             btnMainH = new Button();
-            btnStart = new Button();
             radioButton1 = new RadioButton();
             radioButton2 = new RadioButton();
             radioButton3 = new RadioButton();
             radioButton4 = new RadioButton();
             label1 = new Label();
+            btnNext = new Button();
+            groupBox1 = new GroupBox();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // btnMainH
@@ -54,74 +56,86 @@
             btnMainH.UseVisualStyleBackColor = false;
             btnMainH.Click += btnMainH_Click;
             // 
-            // btnStart
-            // 
-            btnStart.BackColor = Color.DarkGreen;
-            btnStart.FlatAppearance.BorderSize = 0;
-            btnStart.FlatAppearance.MouseDownBackColor = Color.LawnGreen;
-            btnStart.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 192, 0);
-            btnStart.FlatStyle = FlatStyle.Flat;
-            btnStart.ForeColor = SystemColors.ControlLightLight;
-            btnStart.Location = new Point(515, 425);
-            btnStart.Name = "btnStart";
-            btnStart.Size = new Size(128, 49);
-            btnStart.TabIndex = 4;
-            btnStart.Text = "Test start";
-            btnStart.UseVisualStyleBackColor = false;
-            btnStart.Click += btnStart_Click;
-            // 
             // radioButton1
             // 
             radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(474, 327);
+            radioButton1.Location = new Point(146, 211);
             radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(94, 19);
+            radioButton1.Size = new Size(14, 13);
             radioButton1.TabIndex = 5;
             radioButton1.TabStop = true;
-            radioButton1.Text = "radioButton1";
             radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
             radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(617, 327);
+            radioButton2.Location = new Point(270, 211);
             radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(94, 19);
+            radioButton2.Size = new Size(14, 13);
             radioButton2.TabIndex = 6;
             radioButton2.TabStop = true;
-            radioButton2.Text = "radioButton2";
             radioButton2.UseVisualStyleBackColor = true;
             // 
             // radioButton3
             // 
             radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(474, 367);
+            radioButton3.Location = new Point(146, 267);
             radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(94, 19);
+            radioButton3.Size = new Size(14, 13);
             radioButton3.TabIndex = 7;
             radioButton3.TabStop = true;
-            radioButton3.Text = "radioButton3";
             radioButton3.UseVisualStyleBackColor = true;
             // 
             // radioButton4
             // 
             radioButton4.AutoSize = true;
-            radioButton4.Location = new Point(617, 367);
+            radioButton4.Location = new Point(270, 267);
             radioButton4.Name = "radioButton4";
-            radioButton4.Size = new Size(94, 19);
+            radioButton4.Size = new Size(14, 13);
             radioButton4.TabIndex = 8;
             radioButton4.TabStop = true;
-            radioButton4.Text = "radioButton4";
             radioButton4.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(571, 231);
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(37, 106);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(0, 30);
             label1.TabIndex = 9;
-            label1.Text = "label1";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnNext
+            // 
+            btnNext.BackColor = Color.DarkGreen;
+            btnNext.FlatAppearance.BorderSize = 0;
+            btnNext.FlatAppearance.MouseDownBackColor = Color.LawnGreen;
+            btnNext.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 192, 0);
+            btnNext.FlatStyle = FlatStyle.Flat;
+            btnNext.ForeColor = SystemColors.ControlLightLight;
+            btnNext.Location = new Point(173, 332);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(128, 49);
+            btnNext.TabIndex = 11;
+            btnNext.Text = "Next";
+            btnNext.UseVisualStyleBackColor = false;
+            btnNext.Click += btnNext_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            groupBox1.Controls.Add(btnNext);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(radioButton4);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(413, 100);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(465, 401);
+            groupBox1.TabIndex = 12;
+            groupBox1.TabStop = false;
             // 
             // Haupt
             // 
@@ -129,18 +143,14 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.Background;
             ClientSize = new Size(1264, 681);
-            Controls.Add(label1);
-            Controls.Add(radioButton4);
-            Controls.Add(radioButton3);
-            Controls.Add(radioButton2);
-            Controls.Add(radioButton1);
-            Controls.Add(btnStart);
+            Controls.Add(groupBox1);
             Controls.Add(btnMainH);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Haupt";
             Text = "Hauptstädten Quiz";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -152,5 +162,7 @@
         private RadioButton radioButton3;
         private RadioButton radioButton4;
         private Label label1;
+        private Button btnNext;
+        private GroupBox groupBox1;
     }
 }
--- GeoQuiz/GeoQuiz/Haupt.cs
+++ GeoQuiz/GeoQuiz/Haupt.cs
@@ -13,8 +13,14 @@ namespace GeoQuiz
     public partial class Haupt : Form
     {
         private List<Laendern> RL; // Assuming this is your list of countries
-
         private int currentIndex = 0; // Track the current index of the country
+        private List<int> recentlyAskedIndices = new List<int>(); // Track recently asked questions
+        private const int maxRecentQuestions = 4; // Number of recent questions to track to avoid repetition
+        private bool isCheckingAnswer = false; // Flag to check if we are in "check answer" mode
+        private string correctCountry;
+        private int currentQuestionCount = 0;
+        private int score = 0; // Track score
+
 
         public Haupt()
         {
@@ -26,27 +32,89 @@ namespace GeoQuiz
             Close();
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (isCheckingAnswer)
+            {
+                CheckAnswer();
+            }
+            else
+            {
+                LoadNewQuestion();
+            }
+        }
+
+        private void Shuffle<T>(List<T> list)
+        {
+            Random rand = new Random();
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                int j = rand.Next(0, i + 1);
+                T temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+
+        private void LoadNewQuestion()
         {
             Datenbank db = new Datenbank();
 
             int numberOfRandomCountries = 4;
 
-            List<Laendern> randomCountries = db.getRandom(numberOfRandomCountries);
+            RL = db.getRandom(numberOfRandomCountries);
 
             // Ensure there are countries to display
-            if (randomCountries.Count >= 4)
+            if (RL.Count >= 4)
             {
-                // Display the first country's question
-                label1.Text = $"Welche ist die Hauptstadt von {randomCountries[currentIndex].Landname}?";
+                // Generate a random index for the correct answer
+                Random rand = new Random();
+                int newIndex;
+                do
+                {
+                    newIndex = rand.Next(0, numberOfRandomCountries);
+                } while (recentlyAskedIndices.Contains(newIndex));
 
-                // Assign each country's capital to radio buttons
-                radioButton1.Text = randomCountries[0].Landhauptstadt;
-                radioButton2.Text = randomCountries[1].Landhauptstadt;
-                radioButton3.Text = randomCountries[2].Landhauptstadt;
-                radioButton4.Text = randomCountries[3].Landhauptstadt;
+                currentIndex = newIndex;
 
-                // Store the correct country's name
+                recentlyAskedIndices.Add(currentIndex);
+                if (recentlyAskedIndices.Count > maxRecentQuestions)
+                {
+                    recentlyAskedIndices.RemoveAt(0);
+                }
+
+                // Display the question for the randomly selected correct answer
+                label1.Text = $"Welche ist die Hauptstadt von {RL[currentIndex].Landname}?";
+
+                // Shuffle the list of capitals to assign to radio buttons
+                List<string> capitals = new List<string>();
+                for (int i = 0; i < RL.Count; i++)
+                {
+                    capitals.Add(RL[i].Landhauptstadt);
+                }
+
+                // Shuffle the capitals list
+                Shuffle(capitals);
+
+                // Assign shuffled capitals to radio buttons
+                radioButton1.Text = capitals[0];
+                radioButton2.Text = capitals[1];
+                radioButton3.Text = capitals[2];
+                radioButton4.Text = capitals[3];
+
+                // Clear any previous selections
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                radioButton4.Checked = false;
+
+                // Update the correct country variable
+                correctCountry = RL[currentIndex].Landhauptstadt;
+
+                // Set flag to check the answer next
+                isCheckingAnswer = true;
+                btnNext.Text = "Check Answer";
             }
             else
             {
@@ -54,7 +122,59 @@ namespace GeoQuiz
             }
         }
 
+        private void CheckAnswer()
+        {
+            // Check if a radio button is checked
+            if (radioButton1.Checked || radioButton2.Checked || radioButton3.Checked || radioButton4.Checked)
+            {
+                string selectedCapital = "";
 
+                // Determine which radio button is checked
+                if (radioButton1.Checked) selectedCapital = radioButton1.Text;
+                else if (radioButton2.Checked) selectedCapital = radioButton2.Text;
+                else if (radioButton3.Checked) selectedCapital = radioButton3.Text;
+                else if (radioButton4.Checked) selectedCapital = radioButton4.Text;
+
+                // Check if the selected capital matches the correct answer
+                if (!string.IsNullOrEmpty(selectedCapital))
+                {
+                    string correctCapital = RL[currentIndex].Landhauptstadt;
+                    if (selectedCapital == correctCapital)
+                    {
+                        MessageBox.Show("Richtig!");
+                        score++;
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Falsch! Das Antwort ist {correctCapital}.");
+                    }
+
+                    // Set flag to load a new question next
+                    isCheckingAnswer = false;
+                    btnNext.Text = "Next";
+
+                    currentQuestionCount++;
+
+                    if (currentQuestionCount >= maxRecentQuestions)
+                    {
+                        MessageBox.Show($"Round finished! Your score: {score}/{maxRecentQuestions}");
+                        ResetRound();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bitte Wähle eine Antwort");
+            }
+        }
+
+        private void ResetRound()
+        {
+            currentQuestionCount = 0;
+            score = 0;
+            recentlyAskedIndices.Clear();
+            btnNext.Text = "Start New Round";
+        }
 
     }
 }
